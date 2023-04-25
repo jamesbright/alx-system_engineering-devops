268b0c934a83ce7507244587dfcf667917386dfa
James Ononiwu <jamesononiwu@gmail.com>
2023-04-25 08:11:17 -0700

    add puppet config

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,16 @@
+# puppet config server
+
+package { 'nginx':
+provider => 'apt',
+}
+exec {'hello_page':
+command => '/usr/bin/sudo /bin/echo Hello World! > /var/www/html/index.nginx-debian.html',
+}
+exec {'redirect_page':
+
+command => '/usr/bin/sudo /bin/sed -i "66i rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4/ permanent;" /etc/nginx/sites-available/default',
+}
+exec {'start_server':
+
+command => '/usr/bin/sudo /usr/sbin/service nginx start',
+}
