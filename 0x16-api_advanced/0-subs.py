bea65233c784822ed84c313601aa39b1f5002f21
jamesbright <jamesononiwu@gmail.com>
2023-06-07 14:15:36 -0700

    fixed pycodestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,10 +5,12 @@ import sys
 
 
 def number_of_subscribers(subreddit):
-    """function that queries the Reddit API and returns the number of subscri    bers for a given subreddit."""
+    """function that queries the Reddit API
+    and returns the number of subscri    bers for a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    json_obj = requests.get(url, headers={'User-Agent': 'My User Agent 1.0'})
-    if  json_obj.status_code != 404:
+    json_obj = requests.get(url,
+                            headers={'User-Agent': 'My User Agent 1.0'})
+    if json_obj.status_code != 404:
         dict_obj = json_obj.json()
         return dict_obj.get('data').get('subscribers')
     else:
