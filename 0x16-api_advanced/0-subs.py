70e79472842d2fba52013798f62b11731b8c82da
jamesbright <jamesononiwu@gmail.com>
2023-06-07 14:04:23 -0700

    refactored

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""Module that queries the Reddit API and return the number of 
-subscribers a given subreddit has."""
+"""Module with number_of_subscribers function"""
 import requests
+import sys
+
+
 def number_of_subscribers(subreddit):
-    """Function that sends a request to the Reddit API and takes a 
-    subreddit as parameter, then displays the number of subscribers 
-    of the subreddit. subreddit: the subredit to query"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit) 
-    response = requests.get(url, headers={'user-agent': 'request'},
-                            allow_redirects=False) 
-    if response.status_code != requests.codes.ok:
-	return 0
-    rdict = response.json()
-    sub_num = rdict.get('data').get('subscribers')
-    return sub_num
+    """function that queries the Reddit API and returns the number of subscri    bers for a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    json_obj = requests.get(url, headers={'User-Agent': 'My User Agent 1.0'})
+    if  json_obj.status_code != 404:
+        dict_obj = json_obj.json()
+        return dict_obj.get('data').get('subscribers')
+    else:
+        return 0
